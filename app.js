501e2d9cd60413f2384a80e874a5282e4372ca3f
Maxim Alayev <neurologicalstyle@gmail.com>
2021-12-01 22:30:52 -0800

    Setup create Event and create user endpoint

--- app.js
+++ app.js
@@ -1,18 +1,99 @@
 const express = require('express');
 const cors = require('cors');
-
-require('dotenv').config();
+const pool = require('./db');
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+require('dotenv').config();
 
 app.use(
   cors({
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
   }),
+  express.json(),
 );
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.listen(PORT, () => {});
+// ROUTES //
+// Create Event Endpoint
+app.post('/users/create', async (req, res) => {
+  try {
+    const {
+      name,
+      ntype,
+      location,
+      startDateTime,
+      endDateTime,
+      volunteerType,
+      volunteerRequirements,
+      volunteerCapacity,
+      fileAttachments,
+      notes,
+    } = req.body;
+    const createEvent = await pool.query(
+      'INSERT INTO users(name, ntype, location, startDateTime, endDateTime, volunteerType, volunteerCapacity, fileAttachments, notes) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;',
+      [
+        name,
+        ntype,
+        location,
+        startDateTime,
+        endDateTime,
+        volunteerType,
+        volunteerRequirements,
+        volunteerCapacity,
+        fileAttachments,
+        notes,
+      ],
+    );
+    res.json(createEvent.rows[0]);
+  } catch (err) {
+    res.json();
+  }
+});
+// Create User Endpoint
+app.post('/users/create', async (req, res) => {
+  try {
+    const {
+      type,
+      name,
+      birthdate,
+      email,
+      phone,
+      preferContactMethod,
+      city,
+      physicalAddress,
+      weightLiftingAbility,
+      criminalDUIHistory,
+      criminalDUIHistoryDetails,
+      completedChowmatchTraining,
+      foodRunsInterest,
+      specializations,
+      volunteeringRolesInterest,
+      additionalInformation,
+    } = req.body;
+    const createUser = await pool.query(
+      'INSERT INTO events(type, name, birthdate, email, phone, preferContactMethod, city, physicalAddress, weightLiftingAbility, criminalDUIHistory, criminalDUIHistoryDetails, completedChowmatchTraining, foodRunsInterest, specializations, volunteeringRolesInterest, additionalInformation) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *;',
+      [
+        type,
+        name,
+        birthdate,
+        email,
+        phone,
+        preferContactMethod,
+        city,
+        physicalAddress,
+        weightLiftingAbility,
+        criminalDUIHistory,
+        criminalDUIHistoryDetails,
+        completedChowmatchTraining,
+        foodRunsInterest,
+        specializations,
+        volunteeringRolesInterest,
+        additionalInformation,
+      ],
+    );
+    res.json(createUser.rows[0]);
+  } catch (err) {
+    res.json();
+  }
 });
