c191f6f585e7ec7e3e5fc367bc8b3c33b3a3c033
Madhu2244 <madhuxsharma@gmail.com>
2021-12-24 20:02:51 -0600

    Set up Create Event Endpoint

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-// ROUTES //
-
 // Create Event Endpoint
 app.post('/users/create', async (req, res) => {
   try {
@@ -51,54 +49,6 @@ app.post('/users/create', async (req, res) => {
       ],
     );
     res.json(createEvent.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-// Create User Endpoint
-app.post('/users/create', async (req, res) => {
-  try {
-    const {
-      type,
-      name,
-      birthdate,
-      email,
-      phone,
-      preferContactMethod,
-      city,
-      physicalAddress,
-      weightLiftingAbility,
-      criminalDUIHistory,
-      criminalDUIHistoryDetails,
-      completedChowmatchTraining,
-      foodRunsInterest,
-      specializations,
-      volunteeringRolesInterest,
-      additionalInformation,
-    } = req.body;
-    const createUser = await pool.query(
-      'INSERT INTO events(type, name, birthdate, email, phone, preferContactMethod, city, physicalAddress, weightLiftingAbility, criminalDUIHistory, criminalDUIHistoryDetails, completedChowmatchTraining, foodRunsInterest, specializations, volunteeringRolesInterest, additionalInformation) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *;',
-      [
-        type,
-        name,
-        birthdate,
-        email,
-        phone,
-        preferContactMethod,
-        city,
-        physicalAddress,
-        weightLiftingAbility,
-        criminalDUIHistory,
-        criminalDUIHistoryDetails,
-        completedChowmatchTraining,
-        foodRunsInterest,
-        specializations,
-        volunteeringRolesInterest,
-        additionalInformation,
-      ],
-    );
-    res.json(createUser.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
