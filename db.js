30bc35ed7e6c6c338fe4d772e5ea22f748c212a9
henryg0 <hgip@uci.edu>
2022-01-07 12:38:59 -0800

    updated db to use pool instead of client

--- db.js
+++ db.js
@@ -1,9 +1,27 @@
 // need to update username + password + dbname
-const { Client } = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const connString = `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.AWS_DB_NAME}`;
-const client = new Client(connString);
-client.connect();
+const pool = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.AWS_DB_NAME,
+});
 
-module.exports = client;
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error(`client error ${err.stack}`, err.stack);
+  }
+  client.query('SELECT NOW()', (clientErr, result) => {
+    release();
+    if (clientErr) {
+      return console.error('Error executing query', clientErr.stack);
+    }
+    return console.log('should be a timestamp:', result.rows);
+  });
+  return console.log('processed');
+});
+
+module.exports = pool;
