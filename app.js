a0609d1c761b4c0b5388f7fcf537d41317c66d91
Madhu Sharma <madhuxsharma@gmail.com>
2022-01-26 13:22:28 -0800

    Added get available volunteers by hours endpoint

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 require('dotenv').config();
 
+const volunteerRouter = require('./routes/volunteers/volunteers');
+
 app.use(
   cors({
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
   }),
   express.json(),
 );
+
+app.use('/volunteers', volunteerRouter);
 
 app.listen(PORT, () => {});
---
+++ routes/volunteers/volunteers.js
@@ -0,0 +1,24 @@
+// endpoints related to volunteer hours
+const express = require('express');
+const pool = require('../../db');
+
+const volunteerRouter = express();
+
+volunteerRouter.use(express.json());
+
+volunteerRouter.get('/available', async (req, res) => {
+  try {
+    // get params (hour and day)
+    const { endTime, startTime, day } = req.body;
+    // assume startTime and endTime is a timestamp
+    const volunteers = await pool.query(
+      'SELECT u.name FROM availability a INNER JOIN "user" u on u.id = a.user_id WHERE day_of_week = $1 and start_time = $2 and end_time = $3',
+      [day, startTime, endTime],
+    );
+    res.status(200).json(volunteers.rows);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = volunteerRouter;
