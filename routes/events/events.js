4c79eb91fef3e8a68377a3105fa81aab52025a2b
Celine Kim <88957365+scelinekim@users.noreply.github.com>
2022-02-02 01:55:17 -0800

    updated events.js to the right place

--- routes/events/events.js
+++ routes/events/events.js
@@ -55,4 +55,39 @@ eventRouter.post('/create', async (req, res) => {
   }
 });
 
+eventRouter.get('/events/update', async (req, res) => {
+  try {
+    const {
+      name,
+      ntype,
+      location,
+      startDateTime,
+      endDateTime,
+      volunteerType,
+      volunteerRequirements,
+      volunteerCapacity,
+      fileAttachments,
+      notes,
+    } = req.body;
+    const updateEvent = await pool.query(
+      'UPDATE events(name, ntype, location, startDateTime, endDateTime, volunteerType, volunteerCapacity, fileAttachments, notes) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;',
+      [
+        name,
+        ntype,
+        location,
+        startDateTime,
+        endDateTime,
+        volunteerType,
+        volunteerRequirements,
+        volunteerCapacity,
+        fileAttachments,
+        notes,
+      ],
+    );
+    res.status(200).json(updateEvent);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = eventRouter;
