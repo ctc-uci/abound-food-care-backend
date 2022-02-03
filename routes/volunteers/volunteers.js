036f06c302768061539e4257081b89a0f695d95a
Kasey Chuang <chuangky@uci.edu>
2022-02-03 11:30:39 -0800

    Update query.

--- routes/volunteers/volunteers.js
+++ routes/volunteers/volunteers.js
@@ -12,7 +12,7 @@ volunteerRouter.get('/available', async (req, res) => {
     const { endTime, startTime, day } = req.body;
     // assume startTime and endTime is a timestamp
     const volunteers = await pool.query(
-      'SELECT u.name FROM availability a INNER JOIN "user" u on u.id = a.user_id WHERE day_of_week = $1 and start_time = $2 and end_time = $3',
+      'SELECT u.name FROM availability a INNER JOIN "users" u on u.id = a.user_id WHERE day_of_week = $1 and start_time = $2 and end_time = $3',
       [day, startTime, endTime],
     );
     res.status(200).json(volunteers.rows);
