aa11ea1e753b64a499680597dd20872952d1d72a
Kasey Chuang <chuangky@uci.edu>
2022-02-16 11:07:44 -0800

    Fix eslint errors.

--- routes/volunteer_hours/volunteer_hours.js
+++ routes/volunteer_hours/volunteer_hours.js
@@ -9,20 +9,19 @@ hoursRouter.use(express.json());
 // createSubmittedHours
 hoursRouter.post('/create', async (req, res) => {
   try {
-    const { user_id, event_id, start_datetime, end_datetime, approved, notes } = req.body;
+    const { userId, eventId, startDatetime, endDatetime, approved, notes } = req.body;
     // calculate number of hours
     const start = new Date(start_datetime);
     const end = new Date(end_datetime);
     const diff = end.getTime() - start.getTime();
-    const num_hours = diff / (60000 * 60);
+    const numHours = diff / (60000 * 60);
 
     const createdHours = await pool.query(
       'INSERT INTO volunteer_hours(user_id, event_id, start_datetime, end_datetime, approved, num_hours, notes, submitted) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;',
-      [user_id, event_id, start_datetime, end_datetime, approved, num_hours, notes, true],
+      [userId, eventId, startDatetime, endDatetime, approved, numHours, notes, true],
     );
     res.status(200).json(createdHours.rows[0]);
   } catch (err) {
-    console.log(err);
     res.status(400).json(err);
   }
 });
@@ -50,6 +49,5 @@ hoursRouter.get('/submitted', async (req, res) => {
     res.status(400).json(err);
   }
 });
-
 
 module.exports = hoursRouter;
