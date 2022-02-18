4e1c36e28037229c8f09540a6c8e402b81d6d912
henryg0 <hgip@uci.edu>
2022-02-17 19:34:20 -0800

    added /available endpoint (#62)
    Co-authored-by: Rashmi Sharma <rashmisharma489@gmail.com>

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 require('dotenv').config();
 
-app.use(
-  cors({
-    origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
-  }),
-  express.json(),
-);
+const volunteerRouter = require('./routes/volunteers/volunteers');
+
+const corsOptions = {
+  origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
+  credentials: true, // access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.options('*', cors());
 
 // routers
 app.use('/hours', hoursRouter);
