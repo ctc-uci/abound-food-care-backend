229d010b5661905667123fb11cc3e384112bc02b
Rashmi Sharma <rashmisharma489@gmail.com>
2022-02-20 14:11:15 -0800

    updating route to return count of users at event

--- routes/volunteers/volunteers.js
+++ routes/volunteers/volunteers.js
@@ -52,20 +52,15 @@ volunteerRouter.get('/:eventId', async (req, res) => {
   const { eventId } = req.params;
   try {
     const volunteers = await pool.query(
-      `SELECT *
+      `SELECT COUNT(users.id)
         FROM volunteer_at_events
         INNER JOIN users ON volunteer_at_events.user_id = users.id
         AND volunteer_at_events.event_id=$1 AND users.u_type = 'volunteer';`,
       [eventId],
     );
-
-    if (volunteers.rowCount === 0) {
-      res.status(400).json();
-    } else {
-      res.status(200).json(volunteers.rows);
-    }
+    res.status(200).json(volunteers.rows[0]);
   } catch (err) {
-    console.log(err.message);
+    res.status(500).json(err.message);
   }
 });
 
