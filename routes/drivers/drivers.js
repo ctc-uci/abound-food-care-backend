39137d19310af388f365ab16433bb33f80f3802e
henryg0 <hgip@uci.edu>
2022-02-22 04:30:01 -0800

    added router, changed routes, updated err

--- routes/drivers/drivers.js
+++ routes/drivers/drivers.js
@@ -7,28 +7,28 @@ const pool = require('../../db');
 driverRouter.use(express.json());
 
 // Create Driver Endpoint
-driverRouter.post('/driver/create', async (req, res) => {
+driverRouter.post('/create', async (req, res) => {
   try {
     const { userId, vehicleType, distance } = req.body;
     const createDriver = await pool.query(
       'INSERT INTO driver(user_id, vehicle_type, distance) VALUES($1, $2, $3) RETURNING *;',
       [userId, vehicleType, distance],
     );
-    res.json(createDriver.rows[0]);
+    res.status(200).send(createDriver.rows[0]);
   } catch (err) {
-    res.json();
+    res.status(400).send(err);
   }
 });
 
 // Get Driver By Id Endpoint
-driverRouter.get('/driver/:id/get', async (req, res) => {
+driverRouter.get('/:id', async (req, res) => {
   try {
     const getDriverById = await pool.query('SELECT * FROM driver WHERE user_id = $1;', [
       req.params.id,
     ]);
-    res.status(200).json(getDriverById.rows);
+    res.status(200).send(getDriverById.rows[0]);
   } catch (err) {
-    res.status(400).json();
+    res.status(400).send(err);
   }
 });
 
