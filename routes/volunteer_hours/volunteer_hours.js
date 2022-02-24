fffad1e07a7053faf55c7ac193e69c936471ada4
Rashmi Sharma <rashmisharma489@gmail.com>
2022-02-23 21:26:02 -0800

    adding numHours calculation

--- routes/volunteer_hours/volunteer_hours.js
+++ routes/volunteer_hours/volunteer_hours.js
@@ -18,32 +18,35 @@ hoursRouter.get('/unapproved', async (req, res) => {
   }
 });
 
-// Get hours by volunteer
+// Create hours by volunteer
 hoursRouter.post('/create', async (req, res) => {
-  console.log(req.body);
   try {
-    const { userId, eventId, startDateTime, endDatetime, approved, numHours, notes, submitted } =
-      req.body;
+    const { userId, eventId, startDatetime, endDatetime, approved, notes, submitted } = req.body;
+    // calculate numHours
+    const start = new Date(startDatetime);
+    const end = new Date(endDatetime);
+    const diff = end.getTime() - start.getTime();
+    const numHours = parseInt(diff / (60000 * 60), 10);
     const createHoursResponse = await pool.query(
       `INSERT INTO volunteer_hours(
         user_id,
         event_id,
         start_datetime,
         end_datetime,
-        approved,
         num_hours,
+        approved,
         notes,
         submitted)
         VALUES($1, $2, $3, $4, $5, $6, $7, $8)
         RETURNING *`,
-      [userId, eventId, startDateTime, endDatetime, approved, numHours, notes, submitted],
+      [userId, eventId, startDatetime, endDatetime, numHours, approved, notes, submitted],
     );
 
     if (createHoursResponse.rowCount === 0) {
-      res.status(400).send();
+      res.status(400).json();
     } else {
       const hours = createHoursResponse.rows;
-      res.status(200).send(hours);
+      res.status(200).json(hours);
     }
   } catch (err) {
     res.status(500).json(err.message);
