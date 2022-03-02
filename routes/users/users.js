a84772852e499563de56611ef2183ca287c8410f
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-03-02 14:11:50 -0600

    create-user-endpoint (#24)
    * Setup postgres/express/node credentials
    
    * Setup database credentials
    
    * Fixed eslint
    
    * Create PR for #8
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Setup create Event and create user endpoint
    
    * Set up create event endpoint
    
    * Fixed eslint version
    
    * Set up Create Event Endpoint
    
    * hotfix eslint reviewdog issue (#16)
    
    * Create PR for #15
    
    * Manually install eslint using yarn
    
    Co-authored-by: Vakhshoori101 <Vakhshoori101@users.noreply.github.com>
    Co-authored-by: Rostam Vakhshoori <52270944+Vakhshoori101@users.noreply.github.com>
    
    * Create PR for #19
    
    * updated db.js to connect to AWS
    
    * Create PR for #10
    
    * Rebase off of dev
    
    * updated db to use pool instead of client
    
    * updated db to use pool instead of client
    
    * updated db to use pool instead of client
    
    * Create PR for #9
    
    * add test.txt file
    
    * Fixing file structure
    
    * added create user endpoint
    
    * added driving boolean
    
    * aws_username
    
    * removed console, test.txt
    
    Co-authored-by: Madhu2244 <Madhu2244@users.noreply.github.com>
    Co-authored-by: Madhu2244 <madhuxsharma@gmail.com>
    Co-authored-by: Maxim Alayev <neurologicalstyle@gmail.com>
    Co-authored-by: Vakhshoori101 <Vakhshoori101@users.noreply.github.com>
    Co-authored-by: Rostam Vakhshoori <52270944+Vakhshoori101@users.noreply.github.com>
    Co-authored-by: henryg0 <henryg0@users.noreply.github.com>
    Co-authored-by: henryg0 <hgip@uci.edu>
    Co-authored-by: kaseychuang <kaseychuang@users.noreply.github.com>
    Co-authored-by: Kasey Chuang <chuangky@uci.edu>
    Co-authored-by: OliviaIh <OliviaIh@users.noreply.github.com>
    Co-authored-by: David Lu <davidlu5010@gmail.com>
    Co-authored-by: David Lu <dlu@lyrid.io>

--- routes/users/users.js
+++ routes/users/users.js
@@ -6,6 +6,60 @@ const userRouter = express();
 
 userRouter.use(express.json());
 
+// create user endpoint
+userRouter.post('/create', async (req, res) => {
+  try {
+    const {
+      uType,
+      name,
+      birthdate,
+      email,
+      phone,
+      preferredContactMethod,
+      city,
+      physicalAddress,
+      weightLiftingAbility,
+      criminalHistory,
+      duiHistory,
+      duiHistoryDetails,
+      criminalHistoryDetails,
+      completedChowmatchTraining,
+      canDrive,
+      foodRunsInterest,
+      specializations,
+      volunteeringRolesInterest,
+      additionalInformation,
+    } = req.body;
+
+    const createUser = await pool.query(
+      'INSERT INTO users(u_type, name, birthdate, email, phone, preferred_contact_method, city, physical_address, weight_lifting_ability, criminal_history, dui_history, dui_history_details, criminal_history_details, completed_chowmatch_training, can_drive, food_runs_interest, specializations, volunteering_roles_interest, additional_information) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19) RETURNING *;',
+      [
+        uType,
+        name,
+        birthdate,
+        email,
+        phone,
+        preferredContactMethod,
+        city,
+        physicalAddress,
+        weightLiftingAbility,
+        criminalHistory,
+        duiHistory,
+        duiHistoryDetails,
+        criminalHistoryDetails,
+        completedChowmatchTraining,
+        canDrive,
+        foodRunsInterest,
+        specializations,
+        volunteeringRolesInterest,
+        additionalInformation,
+      ],
+    );
+    res.status(200).json(createUser.rows);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 // Get user by id endpoint
 userRouter.get('/:id', async (req, res) => {
   try {
