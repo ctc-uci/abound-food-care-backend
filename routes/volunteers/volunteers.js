d73514ee96c513455a41c427ffa4afbf31d93cc2
Maxim Alayev <neurologicalstyle@gmail.com>
2022-03-12 22:24:24 -0800

    fixed route

--- routes/volunteers/volunteers.js
+++ routes/volunteers/volunteers.js
@@ -97,9 +97,8 @@ volunteerRouter.get('/getVolunteers/:eventId', async (req, res) => {
   try {
     const volunteers = await pool.query(
       `SELECT *
-        FROM users
-        INNER JOIN volunteer_at_events ON volunteer_at_events.user_id = users.id
-        AND volunteer_at_events.event_id=$1 AND users.u_type = 'volunteer';`,
+        FROM volunteer_at_events
+        LEFT JOIN users ON volunteer_at_events.user_id = users.id AND volunteer_at_events.event_id = $1;`,
       [eventId],
     );
     res.status(200).json(volunteers.rows);
