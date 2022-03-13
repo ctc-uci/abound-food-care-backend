1aaa2ca6315dfcd0a19ce2c251feeedc8c183f6a
Maxim Alayev <neurologicalstyle@gmail.com>
2022-03-12 22:08:13 -0800

    Add get volunteer route

--- routes/volunteers/volunteers.js
+++ routes/volunteers/volunteers.js
@@ -91,4 +91,21 @@ volunteerRouter.get('/:eventId', async (req, res) => {
   }
 });
 
+// get volunteers at specific event
+volunteerRouter.get('/getVolunteers/:eventId', async (req, res) => {
+  const { eventId } = req.params;
+  try {
+    const volunteers = await pool.query(
+      `SELECT *
+        FROM users
+        INNER JOIN volunteer_at_events ON volunteer_at_events.user_id = users.id
+        AND volunteer_at_events.event_id=$1 AND users.u_type = 'volunteer';`,
+      [eventId],
+    );
+    res.status(200).json(volunteers.rows);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+});
+
 module.exports = volunteerRouter;
