1ed1af3ccde3eedfafefceb84635efb8a0810418
Vakhshoori101 <rvakhsho@uci.edu>
2022-04-04 16:17:54 -0700

    comment out unneeded requests

--- routes/volunteer_hours.js
+++ routes/volunteer_hours.js
@@ -90,117 +90,115 @@ volunteerHoursRouter.post('/:userId/:eventId', async (req, res) => {
   }
 });
 
-// submit hours for a volunteer
-
 // submitHours
-volunteerHoursRouter.put('/submit', async (req, res) => {
-  try {
-    const { userId, eventId, startDatetime, endDatetime, approved, notes } = req.body;
-    const start = new Date(startDatetime);
-    const end = new Date(endDatetime);
-    const diff = end.getTime() - start.getTime();
-    const numHours = parseInt(diff / (60000 * 60), 10);
+// volunteerHoursRouter.put('/submit', async (req, res) => {
+//   try {
+//     const { userId, eventId, startDatetime, endDatetime, approved, notes } = req.body;
+//     const start = new Date(startDatetime);
+//     const end = new Date(endDatetime);
+//     const diff = end.getTime() - start.getTime();
+//     const numHours = parseInt(diff / (60000 * 60), 10);
 
-    const createdHours = await db.query(
-      `UPDATE volunteer_hours
-        SET start_datetime = $(startDatetime), end_datetime = $(endDatetime),
-          approved = $(approved), num_hours = $(numHours),
-          notes = $(notes), submitted = true
-        WHERE user_id = $(userId) AND event_id = $(eventId)
-        RETURNING *;
-      `,
-      {
-        userId,
-        eventId,
-        startDatetime,
-        endDatetime,
-        approved,
-        numHours,
-        notes,
-      },
-    );
-    if (createdHours.rows.length === 0) {
-      res.status(400).json();
-    } else {
-      res.status(200).json(createdHours.rows);
-    }
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
+//     const createdHours = await db.query(
+//       `UPDATE volunteer_hours
+//         SET start_datetime = $(startDatetime), end_datetime = $(endDatetime),
+//           approved = $(approved), num_hours = $(numHours),
+//           notes = $(notes), submitted = true
+//         WHERE user_id = $(userId) AND event_id = $(eventId)
+//         RETURNING *;
+//       `,
+//       {
+//         userId,
+//         eventId,
+//         startDatetime,
+//         endDatetime,
+//         approved,
+//         numHours,
+//         notes,
+//       },
+//     );
+//     if (createdHours.rows.length === 0) {
+//       res.status(400).json();
+//     } else {
+//       res.status(200).json(createdHours.rows);
+//     }
+//   } catch (err) {
+//     res.status(500).json(err.message);
+//   }
+// });
 
 // getUnsubmittedHours
-volunteerHoursRouter.get('/unsubmitted', async (req, res) => {
-  try {
-    const unsubmittedHours = await pool.query(
-      'SELECT * FROM volunteer_hours WHERE submitted = False;',
-    );
-    res.status(200).json(unsubmittedHours.rows);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
+// volunteerHoursRouter.get('/unsubmitted', async (req, res) => {
+//   try {
+//     const unsubmittedHours = await pool.query(
+//       'SELECT * FROM volunteer_hours WHERE submitted = False;',
+//     );
+//     res.status(200).json(unsubmittedHours.rows);
+//   } catch (err) {
+//     res.status(500).json(err.message);
+//   }
+// });
 
 // getSubmittedHours
-volunteerHoursRouter.get('/submitted', async (req, res) => {
-  try {
-    const submittedHours = await pool.query(
-      'SELECT * FROM volunteer_hours WHERE submitted = True;',
-    );
-    res.status(200).json(submittedHours.rows);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
+// volunteerHoursRouter.get('/submitted', async (req, res) => {
+//   try {
+//     const submittedHours = await pool.query(
+//       'SELECT * FROM volunteer_hours WHERE submitted = True;',
+//     );
+//     res.status(200).json(submittedHours.rows);
+//   } catch (err) {
+//     res.status(500).json(err.message);
+//   }
+// });
 
 // getUnsubmittedHoursByUserId
-volunteerHoursRouter.get('/unsubmittedUser/:id', async (req, res) => {
-  try {
-    const unsubmittedHours = await pool.query(
-      'SELECT v.start_datetime, v.end_datetime, v.num_hours, v.notes, e.name, e.event_id, v.start_datetime AS date FROM volunteer_hours v, event e WHERE v.event_id = e.event_id AND submitted = False AND user_id = $1 ORDER BY v.start_datetime',
-      [req.params.id],
-    );
-    res.status(200).json(unsubmittedHours.rows);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
+// volunteerHoursRouter.get('/unsubmittedUser/:id', async (req, res) => {
+//   try {
+//     const unsubmittedHours = await pool.query(
+//       'SELECT v.start_datetime, v.end_datetime, v.num_hours, v.notes, e.name, e.event_id, v.start_datetime AS date FROM volunteer_hours v, event e WHERE v.event_id = e.event_id AND submitted = False AND user_id = $1 ORDER BY v.start_datetime',
+//       [req.params.id],
+//     );
+//     res.status(200).json(unsubmittedHours.rows);
+//   } catch (err) {
+//     res.status(500).json(err.message);
+//   }
+// });
 
 // getSubmittedHoursByUserId
-volunteerHoursRouter.get('/submittedUser/:id', async (req, res) => {
-  try {
-    const submittedHours = await pool.query(
-      'SELECT v.approved, v.start_datetime, v.end_datetime, v.num_hours, v.notes, e.name, v.start_datetime AS date FROM volunteer_hours v, event e WHERE v.event_id = e.event_id AND submitted = True AND user_id = $1 ORDER BY v.start_datetime',
-      [req.params.id],
-    );
-    res.status(200).json(submittedHours.rows);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
+// volunteerHoursRouter.get('/submittedUser/:id', async (req, res) => {
+//   try {
+//     const submittedHours = await pool.query(
+//       'SELECT v.approved, v.start_datetime, v.end_datetime, v.num_hours, v.notes, e.name, v.start_datetime AS date FROM volunteer_hours v, event e WHERE v.event_id = e.event_id AND submitted = True AND user_id = $1 ORDER BY v.start_datetime',
+//       [req.params.id],
+//     );
+//     res.status(200).json(submittedHours.rows);
+//   } catch (err) {
+//     res.status(500).json(err.message);
+//   }
+// });
 
 // getVolunteerStatistics
-volunteerHoursRouter.get('/statistics/:id', async (req, res) => {
-  try {
-    const volunteerStats = await pool.query(
-      'SELECT COUNT(v.event_id) as event_count, SUM(v.num_hours) as hours FROM volunteer_hours v WHERE submitted = True AND user_id = $1',
-      [req.params.id],
-    );
-    res.status(200).json(volunteerStats.rows);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
+// volunteerHoursRouter.get('/statistics/:id', async (req, res) => {
+//   try {
+//     const volunteerStats = await pool.query(
+//       'SELECT COUNT(v.event_id) as event_count, SUM(v.num_hours) as hours FROM volunteer_hours v WHERE submitted = True AND user_id = $1',
+//       [req.params.id],
+//     );
+//     res.status(200).json(volunteerStats.rows);
+//   } catch (err) {
+//     res.status(500).json(err.message);
+//   }
+// });
 
-volunteerHoursRouter.get('/unapproved', async (req, res) => {
-  try {
-    const unapprovedHours = await pool.query(
-      'SELECT * FROM volunteer_hours WHERE approved = False AND submitted = True;',
-    );
-    res.status(200).json(unapprovedHours.rows);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
+// volunteerHoursRouter.get('/unapproved', async (req, res) => {
+//   try {
+//     const unapprovedHours = await pool.query(
+//       'SELECT * FROM volunteer_hours WHERE approved = False AND submitted = True;',
+//     );
+//     res.status(200).json(unapprovedHours.rows);
+//   } catch (err) {
+//     res.status(500).json(err.message);
+//   }
+// });
 
 module.exports = volunteerHoursRouter;
