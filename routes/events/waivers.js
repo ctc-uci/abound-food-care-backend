6c3ffea1dd3483ca6f75c3f236204cddc4078d70
Alan Chang <alan@alanchang.xyz>
2022-08-13 14:36:05 -0700

    Add first iteration of get all waivers route

--- routes/events/waivers.js
+++ routes/events/waivers.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const aws = require('aws-sdk');
+const archiver = require('archiver');
+const fs = require('fs');
+const path = require('path');
+const { PassThrough } = require('stream');
 const { pool, db } = require('../../server/db');
 const { isNumeric, keysToCamel } = require('../utils');
 
@@ -20,6 +25,57 @@ waiversRouter.get('/:waiverId', async (req, res) => {
     const { waiverId } = req.params;
     const waiver = await pool.query('SELECT * from waivers WHERE waiver_id = $1', [waiverId]);
     res.status(200).send(keysToCamel(waiver.rows[0]));
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+waiversRouter.post('/download/:eventId', async (req, res) => {
+  try {
+    const { name, volunteerData } = req.body;
+    const multiFilesStream = (s3Bucket, infos) => {
+      // using archiver package to create archive object with zip setting -> level from 0(fast, low compression) to 10(slow, high compression)
+      const archive = archiver('zip', { zlib: { level: 5 } });
+
+      infos.forEach((file) => {
+        // using pass through stream object to wrap the stream from aws s3
+        const passthrough = new PassThrough();
+        s3Bucket
+          .getObject({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: file.filename,
+          })
+          .createReadStream()
+          .pipe(passthrough);
+        // name parameter is the name of the file that the file needs to be when unzipped.
+        archive.append(passthrough, { name: file.output });
+      });
+      return archive;
+    };
+    const s3 = new aws.S3({
+      region: process.env.AWS_REGION,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      signatureVersion: 'v4',
+    });
+    const waiverPaths = volunteerData.map((vol) => ({
+      filename: vol.waiver.split('amazonaws.com/')[1],
+      output: vol.waiverName,
+    }));
+    const mfStream = await multiFilesStream(s3, waiverPaths);
+    const zip = fs.createWriteStream(`${name}-waivers.zip`, { flags: 'w' });
+    mfStream.pipe(zip);
+    mfStream.finalize();
+    console.log('checkpoint 1');
+    res.download(path.join(__dirname, `/../../${name}-waivers.zip`), `${name}-waivers.zip`);
+    console.log('checkpoint 2');
+    console.log('checkpoint 3');
+    // const download = Buffer.from(fs.readFileSync(`${name}-waivers.zip`, 'utf8'), 'base64');
+    console.log('checkpoint 4');
+    // res.end(download);
+    console.log('checkpoint 5');
+    // fs.unlinkSync(path.join(__dirname, `${name}-waivers.zip`));
+    console.log('checkpoint 6');
   } catch (err) {
     res.status(400).send(err.message);
   }
